e8172454fd80e51e48285fd318980c61155be25d
Angel Blazquez <angel.blazquez@etu.u-bordeaux.fr>
2021-10-28 15:31:10 +0200

    Début du nuage piquant

---
+++ Jeu/Assets/Scripts/Trap/SpikeManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeManager : MonoBehaviour
+{
+    int[] sequence;
+
+    public GameObject[] cloudSpikes; 
+
+    public enum SpikeType {instant, delayed, sequence};
+    public SpikeType spikeType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        switch (spikeType)
+        {
+            case SpikeType.instant:
+                instantSpike();
+                break;
+            case SpikeType.delayed:
+
+                break;
+            case SpikeType.sequence:
+
+                break;
+        }
+    }
+
+
+    void instantSpike()
+    {
+        foreach (GameObject g in cloudSpikes) {
+            g.GetComponent<IndividualSpike>().changeState(true);
+        }
+    }
+}
