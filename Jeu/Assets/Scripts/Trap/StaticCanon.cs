df346a57e5fd76ecac24f16c0f348e2bfba827df
Léo PUYASTIER <leo.puyastier@etu.u-bordeaux.fr>
2021-11-11 14:53:50 +0100

    Canon et boulet + refactoring très léger

---
+++ Jeu/Assets/Scripts/Trap/StaticCanon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticCanon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    private bool canFire;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canFire = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canFire)
+        {
+            Instantiate(bulletPrefab, firePoint.position, transform.rotation);
+            canFire = false;
+            StartCoroutine(DelayBullet());
+        }
+    }
+
+    IEnumerator DelayBullet()
+    {
+        yield return new WaitForSeconds(3);
+        canFire = true;
+    }
+}
