bfa27aba693f57d3ca0afa9b1176d2d148a1d816
Blazquez Angel <angel.blazquez@etu.u-bordeaux.fr>
2021-11-14 13:58:25 +0100

    Ajout des pieges changement de vitesse et inversion de gravité

---
+++ Jeu/Assets/Scripts/Trap/Player Effects/Speed Change/SpeedChanger.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedChanger : MonoBehaviour
+{
+    public GameObject player;
+
+    public bool zone = true;
+
+    public float playerSpeed = 400; // Modification possible en un multiplicateur.
+    private float oldSpeed = 400;
+
+    public float boostTime = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldSpeed = player.GetComponent<PlayerMovement>().moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator boostTimeCoroutine()
+    {
+        player.GetComponent<PlayerMovement>().moveSpeed = playerSpeed;
+        yield return new WaitForSeconds(boostTime);
+        player.GetComponent<PlayerMovement>().moveSpeed = oldSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(zone)
+        {
+            player.GetComponent<PlayerMovement>().moveSpeed = playerSpeed;
+        } 
+        else
+        {
+            StartCoroutine(boostTimeCoroutine());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (zone)
+        {
+            player.GetComponent<PlayerMovement>().moveSpeed = oldSpeed;
+        }
+
+    }
+}
