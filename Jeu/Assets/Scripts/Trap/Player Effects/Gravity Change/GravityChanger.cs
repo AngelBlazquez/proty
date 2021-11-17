2bfd51d73be888b2f4a0f200be5981bc6d0d3002
Benji <benjamin.gdln@gmail.com>
2021-11-17 17:16:38 +0100

    essaie d'incorporation de spike dans un bloc

--- Jeu/Assets/Scripts/Trap/Player Effects/Gravity Change/GravityChanger.cs
+++ Jeu/Assets/Scripts/Trap/Player Effects/Gravity Change/GravityChanger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GravityChanger : MonoBehaviour
 {
     //bool etat = false;
+<<<<<<< HEAD
 
     public GameObject player;
 
@@ -27,8 +28,20 @@ public class GravityChanger : MonoBehaviour
             //etat = false;
         }
 
+=======
+>>>>>>> be49101 (essaie d'incorporation de spike dans un bloc)
 
     public GameObject player;
+
+    /*private void jumpReverse()
+    {
+        if (etat)
+        {
+            jumpForce = jumpForce*-1;
+        } else {
+            jumpForce = +jumpForce;
+        }
+    }*/
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
@@ -38,6 +51,7 @@ public class GravityChanger : MonoBehaviour
         {
             player.GetComponent<Rigidbody2D>().gravityScale = -1;
             player.GetComponent<Rigidbody2D>().rotation = 180f;
+            //etat = false;
         }
 
     }
@@ -48,8 +62,12 @@ public class GravityChanger : MonoBehaviour
         {
             player.GetComponent<Rigidbody2D>().gravityScale = 1;
             player.GetComponent<Rigidbody2D>().rotation = 0f;
+<<<<<<< HEAD
         player.GetComponent<Rigidbody2D>().gravityScale = 1;
         player.GetComponent<Rigidbody2D>().rotation = 0f;
+=======
+            //etat = true;
+>>>>>>> be49101 (essaie d'incorporation de spike dans un bloc)
         }
     }
 }
