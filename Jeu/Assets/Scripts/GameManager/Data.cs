4c2fb6d330746912537b90df9929c1a650a9c559
Léo PUYASTIER <leo.puyastier@etu.u-bordeaux.fr>
2021-11-25 15:29:02 +0100

    Début de l'écran de choix des niveaux et début de la sauvegarde des données

---
+++ Jeu/Assets/Scripts/GameManager/Data.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    public int lastLevel;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            SaveData();
+        }
+    }
+
+    public void SaveData() {
+        string dataJson = JsonUtility.ToJson(lastLevel);
+        string path = Application.persistentDataPath + "/LevelData.json";
+        Debug.Log(path);
+        System.IO.File.WriteAllText(path, dataJson); 
+    }
+}
