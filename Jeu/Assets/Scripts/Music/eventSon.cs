14c984cd4788fe0cb8bf629d2b4351224aa5d81f
Felix Von Euw <fvoneuw@IUT.BX1>
2021-12-02 14:58:30 +0100

    Script d'animation event

---
+++ Jeu/Assets/Scripts/Music/eventSon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventSon : MonoBehaviour
+{
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get AudioSource in the gameObject (add AudioSource)
+        source = GetComponent<AudioSource>();
+    }
+
+    /// Play noise in the animation event
+    public void PlaySon(){
+        source.Play();
+    }
+}
