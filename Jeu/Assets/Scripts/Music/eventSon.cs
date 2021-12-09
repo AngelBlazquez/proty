cfc9ecb4b08f17f8b318947d23c375de49eaab8a
Felix Von Euw <fvoneuw@IUT.BX1>
2021-12-09 14:54:34 +0100

    Sons

--- Jeu/Assets/Scripts/Music/eventSon.cs
+++ Jeu/Assets/Scripts/Music/eventSon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class eventSon : MonoBehaviour
 {
+    public AudioClip[] clips; 
     private AudioSource source;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,8 @@ public class eventSon : MonoBehaviour
 
     /// Play noise in the animation event
     public void PlaySon(){
+        int rand = Random.Range(0,clips.Length);
+        source.clip = clips[rand];
         source.Play();
     }
 }
