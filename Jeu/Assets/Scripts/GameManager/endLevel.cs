130a1c1786cc6e42ac46c09a8a83e94a82e844f7
Puyastier Leo <leo.puyastier@etu.u-bordeaux.fr>
2021-12-10 20:57:26 +0100

    Lien niveaux

---
+++ Jeu/Assets/Scripts/GameManager/endLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/**
+ * author Yahn Bervas
+ */
+public class EndLevel : MonoBehaviour
+{
+    private int nextScene;
+    private Timer timer;
+
+    void Start()
+    {
+        nextScene = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextScene);
+            nextScene += 1;
+        }
+    }
+}
