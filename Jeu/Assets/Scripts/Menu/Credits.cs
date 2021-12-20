a69fad37500ef2a2c24343e2216e63026aea07c4
Vincent Guyot <guyot.vincent18@gmail.com>
2021-12-20 16:00:47 +0100

    credit

---
+++ Jeu/Assets/Scripts/Menu/Credits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCredits : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void Update()
+    {
+        Debug.Log("Skip Credits");
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            LoadMenu();
+            Debug.Log("Skip Credits");
+        }
+    }
+}
