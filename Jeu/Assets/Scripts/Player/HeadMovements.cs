c127f2ffac4ee0bd685b8253eacc63f01689132a
Benji <benjamin.gdln@gmail.com>
2022-01-06 15:51:44 +0100

    assembling player

---
+++ Jeu/Assets/Scripts/Player/HeadMovements.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadMovements : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D playerRb;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private bool isJumping;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField]
+    private bool isOnGround;
+    private Dictionary<string, KeyCode> Keys = new Dictionary<string, KeyCode>();
+
+    void Start()
+    {
+        Keys.Add("LeftButton", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("LeftButton","LeftArrow")));
+        Keys.Add("RightButton", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("RightButton","RightArrow")));
+        Keys.Add("RunButton", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("RunButton","B")));
+        Keys.Add("JumpButton", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("JumpButton","Space")));
+        Keys.Add("PauseButton", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("PauseButton","Escape")));
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement;
+
+        if (Input.GetKey(Keys["LeftButton"]))
+        {
+            horizontalMovement = Vector3.left.x * moveSpeed * Time.fixedDeltaTime;
+        } else if (Input.GetKey(Keys["RightButton"]))
+        {
+            horizontalMovement = Vector3.right.x * moveSpeed * Time.fixedDeltaTime;
+        } else {
+            horizontalMovement = 0;
+        }
+
+        if (Input.GetKeyDown(Keys["JumpButton"]))
+        {
+            isJumping = true;
+        }
+
+        MovePlayer(horizontalMovement);
+        //FlipPlayer();
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        //Vector3 targetVelocity = new Vector2(_horizontalMovement, playerRb.velocity.y);
+        //playerRb.velocity = Vector3.SmoothDamp(playerRb.velocity, targetVelocity, ref velocity, .05f);
+
+        playerRb.MoveRotation(playerRb.rotation + _horizontalMovement);
+
+        if (isJumping && isOnGround)
+        {
+            playerRb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+
+    void FlipPlayer()
+    {
+        Vector3 PlayerDirection = transform.localScale;
+        if (GetComponent<Rigidbody2D>().rotation != 180)
+        {
+            if (Input.GetKey(Keys["LeftButton"]))
+            {
+                PlayerDirection.x = 1;
+            }
+            else if (Input.GetKey(Keys["RightButton"]))
+            {
+                PlayerDirection.x = -1;
+            }
+        } else
+        {
+            if (Input.GetKey(Keys["RightButton"]))
+            {
+                PlayerDirection.x = 1;
+            }
+            else if (Input.GetKey(Keys["LeftButton"]))
+            {
+                PlayerDirection.x = -1;
+            }
+        }
+            
+        transform.localScale = PlayerDirection;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            isOnGround = false;
+        }
+    }
+
+    public float GetJumpForce()
+    {
+        return jumpForce;
+    }
+
+    public void SetJumpForce(float newJumpForce)
+    {
+        jumpForce = newJumpForce;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+
+    public void SetMoveSpeed(float newMoveSpeed)
+    {
+        moveSpeed = newMoveSpeed;
+    }
+
+    public void SetIsOnGround(bool onGround)
+    {
+        isOnGround = onGround;
+    }
+}
