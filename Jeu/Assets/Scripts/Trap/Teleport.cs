38daf268306d09e120617d697b23d4310cf9f3bd
Léo PUYASTIER <leo.puyastier@etu.u-bordeaux.fr>
2022-01-13 19:15:13 +0100

    Modif du téléporteur

--- Jeu/Assets/Scripts/Trap/Teleport.cs
+++ Jeu/Assets/Scripts/Trap/Teleport.cs
@@ -11,26 +11,33 @@ public class Teleport : MonoBehaviour
 {
     [SerializeField]
     private GameObject otherPoint;
+    [SerializeField]
+    private int delay; // -1 => deactivate
+
     private bool canTeleport = true;
 
     /// <summary>
     /// Teleports the player if the player didn't just teleport
     /// </summary>
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.CompareTag("Player") && canTeleport) {          
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && canTeleport && delay >= 0)
+        {
             Teleport otp = otherPoint.GetComponent<Teleport>();
             otp.DeactivateTeleportation();
             other.gameObject.transform.position = otherPoint.transform.position;
-        } 
+        }
     }
 
-    public void DeactivateTeleportation() {
+    public void DeactivateTeleportation()
+    {
         canTeleport = false;
         StartCoroutine(WaitForTeleportation());
     }
 
-    private IEnumerator WaitForTeleportation(){
-        yield return new WaitForSeconds(5);
+    private IEnumerator WaitForTeleportation()
+    {
+        yield return new WaitForSeconds(delay);
         canTeleport = true;
     }
 }
