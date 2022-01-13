055481c3b12bb45de303d86f1f47a95ea4a045ed
Léo PUYASTIER <leo.puyastier@etu.u-bordeaux.fr>
2022-01-13 14:50:01 +0100

    Début de la pluie

---
+++ Jeu/Assets/Scripts/Trap/ZoneSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Teleports the player to an other point
+/// Made by Léo PUYASTIER
+/// </summary>
+public class ZoneSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject leftPoint;
+    [SerializeField]
+    private GameObject rightPoint;
+    [SerializeField]
+    private GameObject object;
+    [SerializeField]
+    private float spawnTime;
+
+    private bool canSpawn;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canSpawn) 
+        {
+
+        }
+    }
+
+    private IEnumerator WaitForTeleportation(){
+        yield return new WaitForSeconds(1);
+        canSpawn = true;
+    }
+}
