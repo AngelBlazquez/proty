20e9398da9d826a36f4279e1fafedfe5025d54d6
Félix VE <felive4@gmail.com>
2022-03-03 15:08:01 +0100

    scripts pour skin des couvres chefs

---
+++ Jeu/Assets/Scripts/Player/skinHatPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skinHatPlayer : MonoBehaviour
+{
+    public Sprite[] spritesHats;
+    public SpriteRenderer hat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hat.sprite = spritesHats[PlayerPrefs.GetInt("Hat")];
+    }
+
+}
