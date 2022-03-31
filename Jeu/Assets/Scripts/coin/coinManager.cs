79f72d5d1c1b5ea81e38e764356b586cc85bad28
Félix VE <felive4@gmail.com>
2022-03-31 15:05:30 +0200

    ajout des pieces dans moitié des niveaux

--- Jeu/Assets/Scripts/coin/coinManager.cs
+++ Jeu/Assets/Scripts/coin/coinManager.cs
@@ -5,17 +5,25 @@ using TMPro;
 
 public class coinManager : MonoBehaviour
 {
-    public TextMeshProUGUI nbMorts;
+    public TextMeshProUGUI nbCoin;
+
+    private DataManager data;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        data = FindObjectOfType<DataManager>().GetComponent<DataManager>();
+        ShowCoin();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ShowCoin()
+    {
+        nbCoin.text = data.GetCoin().ToString();
     }
 }
