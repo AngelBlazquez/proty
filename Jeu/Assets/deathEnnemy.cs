8167e3d7d5cea22c3e3c6a0f707affc90dbc7480
Yahn Bervas <yahn.bervas@etu.u-bordeaux.fr>
2022-06-03 01:02:33 +0200

    script ennemi qui suit fini et début du script du boss (invocation ennemi qui suit)

---
+++ Jeu/Assets/deathEnnemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathEnnemy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ennemy;
+    [SerializeField]
+    private Collider2D characterCol;
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Player" && col.IsTouching(characterCol))
+            Destroy(ennemy);
+    }
+}
