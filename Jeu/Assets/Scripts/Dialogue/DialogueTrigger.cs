3647e665107fdc0b896149145318a79ed1ec2a7a
Blazquez Angel <angel.blazquez@etu.u-bordeaux.fr>
2022-06-19 02:27:38 +0200

    Systeme de dialogue

---
+++ Jeu/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField]
+    DialogueSystem dialogueSystem;
+
+    private bool oneTimeTrigger = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && !oneTimeTrigger)
+        {
+            dialogueSystem.ShowDialogue();
+            oneTimeTrigger = true;
+        }
+    }
+}
